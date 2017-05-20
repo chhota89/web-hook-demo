887a12980451969a5666682be34428066935b0ff
Muhammad Chhota <chhota89@gmail.com>
2017-05-20 11:11:27 +0530

    Action on Google is added to index.js

--- index.js
+++ index.js
@@ -23,3 +23,37 @@ app.post('/echo', function(req, res){
 var server = app.listen(process.env.PORT || 3000, function () {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+
+const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+
+exports.sayNumber = (request, response) => {
+  const app = new ActionsSdkApp({request, response});
+
+  function mainIntent (app) {
+    console.log('mainIntent');
+    let inputPrompt = app.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
+      'I can read out an ordinal like ' +
+      '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>',
+      ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+    app.ask(inputPrompt);
+  }
+
+  function rawInput (app) {
+    console.log('rawInput');
+    if (app.getRawInput() === 'bye') {
+      app.tell('Goodbye!');
+    } else {
+      let inputPrompt = app.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">'
+        + app.getRawInput() + '</say-as></speak>',
+        ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+      app.ask(inputPrompt);
+    }
+  }
+
+  let actionMap = new Map();
+  actionMap.set(app.StandardIntents.MAIN, mainIntent);
+  actionMap.set(app.StandardIntents.TEXT, rawInput);
+
+  app.handleRequest(actionMap);
+};
